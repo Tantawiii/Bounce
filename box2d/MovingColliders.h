25113586c660db577f798b7f1851f07549831ef1
Mohamed Ehab Shalaby <107648111+Mido-Ehab@users.noreply.github.com>
2024-12-03 16:19:03 +0200

    Update MovingColliders.h

--- box2d/MovingColliders.h
+++ box2d/MovingColliders.h
@@ -6,13 +6,20 @@ class MovingColliders {
 private:
     b2Body* movObs;
 
+    sf::RectangleShape rect;
+    sf::Vector2f s;
     sf::Vector2f startPosition; // Start position as a vector
     sf::Vector2f endPosition;   // End position as a vector
 
     float speed;
+    float rotationAngle;
+
     bool isMovingForward;
     bool isMovingVertical;
 
+
+    sf::Texture texture;
+    sf::Sprite sprite;
 public:
     MovingColliders(b2World& world, sf::Vector2f startPosition, sf::Vector2f range, float speed, bool horizontal, bool vertical);
     void updateColliderX(float deltaTime);
